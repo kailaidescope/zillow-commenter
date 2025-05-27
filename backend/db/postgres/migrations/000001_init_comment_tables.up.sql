f04e5a00eae805939d5f929b4c2877a328a9d3e4
m3lodicaman <kai.turanski@mail.mcgill.ca>
2025-05-27 17:37:52 -0400

    Generated postgres query code

---
+++ backend/db/postgres/migrations/000001_init_comment_tables.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS comments (
+    comment_id SERIAL PRIMARY KEY,
+    listing_id varchar(200) NOT NULL,
+    user_ip varchar(45) NOT NULL,
+    user_id varchar(50) NOT NULL,
+    username varchar(50) NOT NULL,
+    comment_text varchar(300) NOT NULL,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS blacklist (
+    blacklist_id SERIAL PRIMARY KEY,
+    cause varchar(100) NOT NULL,
+    user_ip varchar(45) NOT NULL,
+    user_id varchar(50),
+    username varchar(50),
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
---
+++ backend/db/postgres/sqlc/sql/query.sql
@@ -0,0 +1,9 @@
+-- name: GetCommentsByListingID :many
+SELECT comment_id, listing_id, user_ip, user_id, username, comment_text, EXTRACT(EPOCH FROM date_created) FROM comments
+WHERE listing_id = $1
+ORDER BY date_created DESC;
+
+-- name: PostComment :one
+INSERT INTO comments (listing_id, user_ip, user_id, username, comment_text)
+VALUES ($1, $2, $3, $4, $5)
+RETURNING comment_id, listing_id, user_ip, user_id, username, comment_text, EXTRACT(EPOCH FROM date_created);
