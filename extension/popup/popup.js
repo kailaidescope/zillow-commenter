f926fb7a08b919141361da8d9a1e324ef8cb3d4f
m3lodicaman <kai.turanski@mail.mcgill.ca>
2025-06-06 00:42:40 -0400

    Updated REST API documentation

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,6 +1,9 @@
-const API_ADDRESS = window.localStorage.getItem('zillow_commenter_api_address') || 'localhost';
+/* const API_ADDRESS = window.localStorage.getItem('zillow_commenter_api_address') || 'localhost';
 const API_PORT = "3000";
-const API_URL = `https://${API_ADDRESS}:${API_PORT}/api/v1`;
+const API_URL = `https://${API_ADDRESS}:${API_PORT}/api/v1`; */
+
+const API_ADDRESS = "";
+const API_URL = `${API_ADDRESS}/api/v1`;
 
 
 // Log the current user ID from localStorage
